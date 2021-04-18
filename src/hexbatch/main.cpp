4a02bfb5e901bc6d6b6520e525347248899c7268
Will Woodlief <willwoodlief@gmail.com>
2021-04-18 17:26:14 -0500

    better test file

--- src/hexbatch/main.cpp
+++ src/hexbatch/main.cpp
@@ -1,4 +1,34 @@
-#include <iostream>
+// C program to display hostname
+// and IP address
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// Returns hostname for the local computer
+void checkHostName(int hostname)
+{
+    if (hostname == -1)
+    {
+        perror("gethostname");
+        exit(1);
+    }
+}
+
+// Returns host information corresponding to host name
+void checkHostEntry(struct hostent * hostentry)
+{
+    if (hostentry == nullptr)
+    {
+        perror("gethostbyname");
+        exit(1);
+    }
+}
+
+
+
 //Hexbatch
 
 /// \file
@@ -8,7 +38,28 @@
 /// \param  argv An argument vector of the command line arguments
 /// \return an integer 0 upon exit success
 /// \todo test this out
-int main() {
-    std::cout << "Hello, Hexbatch!" << std::endl;
+int main()
+{
+    char hostbuffer[256];
+    char *IPbuffer;
+    struct hostent *host_entry;
+    int hostname;
+
+    // To retrieve hostname
+    hostname = gethostname(hostbuffer, sizeof(hostbuffer));
+    checkHostName(hostname);
+
+    // To retrieve host information
+    host_entry = gethostbyname(hostbuffer);
+    checkHostEntry(host_entry);
+
+    // To convert an Internet network
+    // address into ASCII string
+    IPbuffer = inet_ntoa(*((struct in_addr*)
+            host_entry->h_addr_list[0]));
+
+    printf("Hostname is: %s\n", hostbuffer);
+    printf("Host IP: %s", IPbuffer);
+
     return 0;
 }
